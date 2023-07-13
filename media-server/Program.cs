99b1186b3ef4e6aa489a1ac62f02ebd227b352ab
nickam1025 <nickam1025@gmail.com>
2023-07-13 16:20:03 -0400

    Added and configured YARP for MyAnimeList

---
+++ media-server/Program.cs
@@ -0,0 +1,18 @@
+namespace MediaServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create a Kestrel web server, and tell it to use the Startup class
+            // for the service configuration
+            var myHostBuilder = Host.CreateDefaultBuilder(args);
+            myHostBuilder.ConfigureWebHostDefaults(webHostBuilder =>
+            {
+                webHostBuilder.UseStartup<Startup>();
+            });
+            var myHost = myHostBuilder.Build();
+            myHost.Run();
+        }
+    }
+}
